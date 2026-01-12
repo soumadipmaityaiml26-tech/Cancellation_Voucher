9f0b034dc0d6ddb6b439978678e042fb56d810b3
sayakghosh <sayakghosh2004sg@gmail.com>
2026-01-12 12:39:36 +0530

    updated

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api/v1",
+  baseURL:
+    import.meta.env.VITE_API_URL ||
+    "https://nayqp7xifyoor6kh67aigarsha0ffrbo.lambda-url.ap-south-1.on.aws/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
